ad45e57477dd1b698d2fad07145b1ec8177f8f9b
DeleonZander <deleonzandereindrex@gmail.com>
2025-03-09 20:28:23 +0800

    Add files via upload

---
+++ ipo4.py
@@ -0,0 +1,15 @@
+grade = int(input("What is your Grade?:"))
+if grade >=101:
+    print("Invalid Grade")
+elif grade >=90:
+    print("Grade A")
+elif grade >=80:
+    print("Grade B")
+elif grade >=70:
+    print("Grade C")
+elif grade >=60:
+    print("Grade D")
+elif grade >=0:
+    print("Grade F")
+else:
+    print("Invalid Grade")
